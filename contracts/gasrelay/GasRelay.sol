70f00924bf905b13caf08e3d6562480b12a4960b
Ricardo Guilherme Schmidt <ricardo3@status.im>
2019-09-18 12:12:07 -0300

    refactor signature prefixes and rgument order

--- contracts/gasrelay/GasRelay.sol
+++ contracts/gasrelay/GasRelay.sol
@@ -10,13 +10,14 @@ import "../token/ERC20Token.sol";
 contract GasRelay {
 
     bytes4 internal constant MSG_CALL_GASRELAY_PREFIX = bytes4(
-        keccak256("callGasRelay(address,uint256,bytes32,uint256,uint256,address,address)")
+        keccak256("callGasRelay(address,uint256,bytes,uint256,address,uint256,uint256,address)")
     );
     bytes4 internal constant MSG_DEPLOY_GASRELAY_PREFIX = bytes4(
-        keccak256("deployGasRelay(uint256,bytes32,uint256,uint256,address,address)")
+        keccak256("deployGasRelay(uint256,bytes,uint256,address,uint256,uint256,address)")
     );
+
     bytes4 internal constant MSG_APPROVEANDCALL_GASRELAY_PREFIX = bytes4(
-        keccak256("approveAndCallGasRelay(address,address,uint256,bytes32,uint256,uint256,address)")
+        keccak256("approveAndCallGasRelay(address,address,uint256,bytes,uint256,address,uint256,uint256)")
     );
 
     string internal constant ERR_BAD_START_GAS = "Bad start gas";
@@ -34,7 +35,7 @@ contract GasRelay {
      * @param _to destination of call
      * @param _value call value (ether)
      * @param _data call data
-     * @param _gasPrice price in SNT paid back to msg.sender for each gas unit used
+     * @param _gasPrice price in SNT paid back to `msg.sender` per gas unit used
      * @param _gasLimit maximum gas of this transacton
      * @param _gasToken token being used for paying `msg.sender`
      * @param _signature rsv concatenated ethereum signed message signatures required
@@ -55,7 +56,7 @@ contract GasRelay {
      *         allows account of being controlled without requiring ether in key balace
      * @param _value call value (ether) to be sent to newly created contract
      * @param _data contract code data
-     * @param _gasPrice price in SNT paid back to msg.sender for each gas unit used
+     * @param _gasPrice price in SNT paid back to `msg.sender` per gas unit used
      * @param _gasLimit maximum gas of this transacton
      * @param _gasToken token being used for paying `msg.sender`
      * @param _signature rsv concatenated ethereum signed message signatures required
@@ -79,7 +80,7 @@ contract GasRelay {
      * @param _to destination of call
      * @param _value call value (in `_baseToken`)
      * @param _data call data
-     * @param _gasPrice price in SNT paid back to msg.sender for each gas unit used
+     * @param _gasPrice price in SNT paid back to `msg.sender` per gas unit used
      * @param _gasLimit maximum gas of this transacton
      * @param _signature rsv concatenated ethereum signed message signatures required
      */
@@ -98,23 +99,23 @@ contract GasRelay {
      * @notice get callHash
      * @param _to destination of call
      * @param _value call value (ether)
-     * @param _dataHash call data hash
+     * @param _data call data
      * @param _nonce current account nonce
-     * @param _gasPrice price in SNT paid back to msg.sender for each gas unit used
+     * @param _gasRelayer beneficiary of gas refund
+     * @param _gasPrice price in SNT paid back to `_gasRelayer` per gas unit used
      * @param _gasLimit maximum gas of this transacton
      * @param _gasToken token being used for paying `_gasRelayer`
-     * @param _gasRelayer beneficiary of gas, if address(0), msg.sender
      * @return callGasRelayHash the hash to be signed by wallet
      */
     function callGasRelayHash(
         address _to,
         uint256 _value,
-        bytes32 _dataHash,
-        uint _nonce,
+        bytes memory _data,
+        uint256 _nonce,
+        address _gasRelayer,
         uint256 _gasPrice,
         uint256 _gasLimit,
-        address _gasToken,
-        address _gasRelayer
+        address _gasToken
     )
         public
         view
@@ -126,12 +127,12 @@ contract GasRelay {
                 MSG_CALL_GASRELAY_PREFIX,
                 _to,
                 _value,
-                _dataHash,
+                _data,
                 _nonce,
+                _gasRelayer,
                 _gasPrice,
                 _gasLimit,
-                _gasToken,
-                _gasRelayer
+                _gasToken
             )
         );
     }
@@ -139,24 +140,24 @@ contract GasRelay {
     function getNonce() external view returns(uint256);
 
     /**
-     * @notice get callHash
-     * @param _value call value (ether)
-     * @param _dataHash call data hash
+     * @notice get deployGasRelayHash
+     * @param _value value (ETH) sent together in deplpy
+     * @param _data contract data
      * @param _nonce current account nonce
-     * @param _gasPrice price in SNT paid back to msg.sender for each gas unit used
+     * @param _gasRelayer beneficiary of gas refund
+     * @param _gasPrice price in SNT paid back to `_gasRelayer` per gas unit used
      * @param _gasLimit maximum gas of this transacton
      * @param _gasToken token being used for paying `_gasRelayer`
-     * @param _gasRelayer beneficiary of gas, if address(0), msg.sender
-     * @return callGasRelayHash the hash to be signed by wallet
+     * @return deployGasRelayHash the hash to be signed by wallet
      */
     function deployGasRelayHash(
         uint256 _value,
-        bytes32 _dataHash,
+        bytes memory _data,
         uint256 _nonce,
+        address _gasRelayer,
         uint256 _gasPrice,
         uint256 _gasLimit,
-        address _gasToken,
-        address _gasRelayer
+        address _gasToken
     )
         public
         view
@@ -167,12 +168,12 @@ contract GasRelay {
                 address(this),
                 MSG_DEPLOY_GASRELAY_PREFIX,
                 _value,
-                _dataHash,
+                _data,
                 _nonce,
+                _gasRelayer,
                 _gasPrice,
                 _gasLimit,
-                _gasToken,
-                _gasRelayer
+                _gasToken
             )
         );
     }
@@ -180,23 +181,24 @@ contract GasRelay {
     /**
      * @notice return approveAndCall Relay Hash
      * @param _baseToken token approved for `_to` and token being used for paying `_gasRelayer`
-     * @param _to destination of call
+     * @param _to call destination
      * @param _value call value (in `_baseToken`)
-     * @param _dataHash call data hash
-     * @param _gasPrice price in SNT paid back to msg.sender for each gas unit used
-     * @param _gasLimit maximum gas of this transacton
-     * @param _gasRelayer beneficiary of gas, if address(0), msg.sender
+     * @param _data call data
+     * @param _nonce current account nonce
+     * @param _gasRelayer beneficiary of gas refund
+     * @param _gasPrice price in SNT paid back to `_gasRelayer` per gas unit used
+     * @param _gasLimit maximum gas of this transaction
      * @return approveAndCallHash the hash to be signed by wallet
      */
     function approveAndCallGasRelayHash(
         address _baseToken,
         address _to,
         uint256 _value,
-        bytes32 _dataHash,
-        uint _nonce,
+        bytes memory _data,
+        uint256 _nonce,
+        address _gasRelayer,
         uint256 _gasPrice,
-        uint256 _gasLimit,
-        address _gasRelayer
+        uint256 _gasLimit
     )
         public
         view
@@ -209,8 +211,9 @@ contract GasRelay {
                 _baseToken,
                 _to,
                 _value,
-                _dataHash,
+                _data,
                 _nonce,
+                _gasRelayer,
                 _gasPrice,
                 _gasLimit,
                 _gasRelayer
@@ -221,7 +224,7 @@ contract GasRelay {
     /**
      * @notice check gas limit and pays gas to relayer
      * @param _startGas gasleft on call start
-     * @param _gasPrice price in SNT paid back to msg.sender for each gas unit used
+     * @param _gasPrice price in SNT paid back to `_gasRelayer` per gas unit used
      * @param _gasLimit maximum gas of this transacton
      * @param _gasToken token being used for paying `_gasRelayer`
      * @param _gasRelayer beneficiary of the payout

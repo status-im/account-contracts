4cf4f3ca178883eba5e20e1bf1ccc5bf6aede4fc
Ricardo Guilherme Schmidt <ricardo3@status.im>
2019-11-28 17:19:49 -0200

    remove trailing space

--- contracts/account/Signer.sol
+++ contracts/account/Signer.sol
@@ -1,7 +1,7 @@
 pragma solidity >=0.5.0 <0.6.0;
 
-/** 
- * @notice ERC-1271: Standard Signature Validation Method for Contracts  
+/**
+ * @notice ERC-1271: Standard Signature Validation Method for Contracts
  */
 contract Signer {
 

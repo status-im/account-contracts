436414f5fa7db2eecdeefc496721ea0fa3523fdd
Ricardo Guilherme Schmidt <ricardo3@status.im>
2020-01-26 01:04:54 -0200

    add fuzzy tester

--- test/multimerkleproof.spec.js
+++ test/multimerkleproof.spec.js
@@ -108,6 +108,67 @@ contract('MultiMerkleProof', function () {
       );
     });
 
+    it('should return true for a valid Merkle proof ids 4', async function () {
+      const elements = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16']; 
+      const merkleTree = new MerkleTree(elements);
+      const leafs = merkleTree.getPairs([elements[0],elements[3],elements[4],elements[15]]);
+      const proof = merkleTree.getMultiProof(leafs);
+      assert(
+        await MerkleMultiProofWrapper.methods.verifyMultiProof(
+          merkleTree.getHexRoot(), 
+          leafs, 
+          proof.map(bufferToHex), 
+          merkleTree.getProofIds(leafs, proof)
+        ).call()
+      );
+    });
+
+    it('should return true for a valid Merkle proof (fuzzy)', async function () {
+      const fullChecks = 1;
+      const proofChecks = 10;
+      const maxTreeSizePow = 8;
+      const maxLeafsSize = 100;
+
+      for(let i = 0; i < fullChecks; i++){
+       
+        const elements = Array.from({length: Math.pow(2,(4+Math.floor(Math.random()*maxTreeSizePow)))}, () => Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15));
+        const merkleTree = new MerkleTree(elements);
+        for(let j = 0; j < proofChecks; j++){
+          const leafs = merkleTree.getPairs(Array.from({length: maxLeafsSize}, () => elements[Math.floor(Math.random()*elements.length)] ).filter((value, index, self) => self.indexOf(value) === index));
+          const proof = merkleTree.getMultiProof(leafs);
+          const result = await MerkleMultiProofWrapper.methods.verifyMultiProof2(
+            merkleTree.getHexRoot(),
+            leafs, 
+            proof.map(bufferToHex), 
+            merkleTree.getProofFlags(leafs, proof)
+          ).call();
+          
+          const resultIds = await MerkleMultiProofWrapper.methods.verifyMultiProof(
+            merkleTree.getHexRoot(), 
+            leafs, 
+            proof.map(bufferToHex), 
+            merkleTree.getProofIds(leafs, proof)
+          ).call();
+
+          if(!result || !resultIds) {
+            console.log("fullchecks", i, "proofChecks", j , "bools", result, "ids", resultIds)
+            console.log("els", merkleTree.bufArrToHex(merkleTree.elements));
+            console.log("leafs", merkleTree.bufArrToHex(leafs));
+            console.log("proof", merkleTree.bufArrToHex(proof));
+
+            if(result){
+              console.log("bools", merkleTree.getProofFlags(leafs, proof, true))
+            } else {
+              console.log("ids", merkleTree.getProofIds(leafs, proof, true))
+            }
+          }
+          assert(
+            result && resultIds
+          );
+        }
+      }
+    });
+
 
     xit('should return false for a Merkle proof of invalid length', async function () {
       const elements = ['a', 'b', 'c'];
